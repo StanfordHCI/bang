e73dff9df9affd47d5b707ec847efc1faab5b6e7
Glitch (bang) <none>
2018-06-08 22:11:34 +0000

    🐑🔔 Updated with Glitch

--- tools.js
+++ tools.js
@@ -15,63 +15,50 @@ const teamChecker = (rounds) => {
 }
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-const randomAnimal = 'Bison Eagle Pony Cow Deer Duck Rabit Spider Wolf Lion Snake Shark Bird Bear Fish Horse Cat Dog'.split(" ").pick()
-const randomAdjective = 'new small young little likely nice cultured snappy spry conventional'.split(" ").pick() 
+const randomAnimal = 'Bison Eagle Pony Cow Deer Duck Rabit Spider Wolf Lion Snake Shark Bird Bear Fish Horse Cat Dog'.split(" ")
+const randomAdjective = 'new small young little likely nice cultured snappy spry conventional'.split(" ")
 
-
-
-module.exports = {
-  letters: letters,
-  createTeams: (teamSize, rounds, people = letters.slice(0,teamSize**2)) => {
+const createTeams = (teamSize, rounds, people) => {
     console.log(people.length != teamSize **2 ? "Wrong number of people" : "Building teams")
-    // if ((teamSize + 1) > rounds.length) {
-    //   console.log("error: need to update createTeams for this size configuration")
-    //   return
-    // }
+    console.log(teamSize > rounds.length+1 ? "Error" : "")
 
     let roundTeams = []
     let collaborators = {}
-    people.forEach( person => { collaborators[person] = [] })
+    people.forEach( person => { collaborators[person] = [person] })
 
-    rounds.forEach(round => { 
-      console.log(round);
-      
+    rounds.forEach(round => {       
       let unUsedPeople = people.slice()
       let teams = []
-      //console.log(unUsedPeople);
+      
       while (unUsedPeople.length) {
-        //console.log(unUsedPeople); 
         let team = []
         let currentPerson = unUsedPeople.pop()  
         team.push(currentPerson)
  
-        while (team.length < teamSize) {
-          
-          // people themselves are never added to teamCollaborators
-          /*
+        while (team.length < teamSize) {  
           let teamCollaborators = team.map(member => collaborators[member] ).reduce((a,b) => a.concat(b) ).set() //find all prior collaborators
           let remainingOptions = unUsedPeople.filter(person => !teamCollaborators.includes(person) ) //find all remaining options
+          if (!remainingOptions.length) { return createTeams(teamSize, rounds, people) }
           let newCollaborator = remainingOptions.pick()
           unUsedPeople = unUsedPeople.filter(person => person != newCollaborator ) //update unused people
-          */
-          
-          let teamCollaborators = team.map(member => collaborators[member] ).reduce((a,b) => a.concat(b) ).set();          
-          
-          //if(!teamCollaborators.includes(person)){
-          //}
+
           team.forEach(member => { //add all new collaborators both ways
-            //collaborators[member].push(newCollaborator)
-            //collaborators[newCollaborator].push(member)
+            collaborators[member].push(newCollaborator)
+            collaborators[newCollaborator].push(member)
           })
-          ///eam.push(newCollaborator) // add new collaborator into the team
+          team.push(newCollaborator) // add new collaborator into the team
         }
         teams.push(team)
       }
       roundTeams.push(teams) 
     })
-    // console.log(collaborators)
     console.log("Built",teamChecker(roundTeams) ? "valid" : "NOT VALID","teams of",teamSize)
     return roundTeams
-  }, 
-  makeName: () => randomAdjective + randomAnimal
+  }
+
+
+module.exports = {
+  letters: letters,
+  createTeams: createTeams, 
+  makeName: () => randomAdjective.pick() + randomAnimal.pick()
 };
