2913aefc7f8c5265a21763f3c3f042f441bc3086
Mark Whiting <mark.whiting@gmail.com>
2018-07-07 10:30:09 -0700

    Documented postSurveyGenerator a little

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ io.on('connection', (socket) => {
       if (currentRound >= numRounds) {
         users.forEach(user => {
           user.ready = false
-          let survey = postSurvey(user)
+          let survey = postSurveyGenerator(user)
           user.results.manipulation = survey.correctAnswer
           db.users.update({ id: socket.id }, {$set: {"results.manipulation": user.results.manipulation}}, {}, (err, numReplaced) => { console.log(err ? err : "Stored manipulation: " + user.name) })
           io.in(user.id).emit('postSurvey', {questions: survey.questions, answers:survey.answers})
@@ -383,8 +383,12 @@ const midSurvey = (user) => {
                             answers:["1. strongly disagree", "2. disagree", "3. neutral", "4. agree", "5. strongly agree"] } }}
 }
 
-const postSurvey = (user) => {
+// This function generates a post survey for a user (listing out each team they were part of), and then provides the correct answer to check against.
+const postSurveyGenerator = (user) => {
+  // Makes a list of teams this user has worked with
   const roomTeams = user.rooms.map((room, rIndex) => { return users.filter(user => user.rooms[rIndex] == room) })
+
+  // Makes a human friendly string for each team with things like 'you' for the current user, commas and 'and' before the last name.
   const answers = roomTeams.map((team, tIndex) => team.reduce((total, current, pIndex, pArr)=>{
     const friend = user.friends.find(friend => friend.id == current.id)
     let name = ((experimentRound == tIndex && currentCondition == "treatment") ? friend.tAlias : friend.alias)
@@ -392,6 +396,7 @@ const postSurvey = (user) => {
     return name + (pIndex == 0 ? "" : ((pIndex + 1) == pArr.length ? " and " : ", ")) + total
   },""))
 
+  // Makes a list comtaining the 2 team same teams, or empty if none.
   let correctAnswer = answers.filter((team,index) => {
     return conditions[currentCondition][index] == experimentRoundIndicator })
   if (correctAnswer.length == 1) {correctAnswer = ""}
@@ -399,6 +404,5 @@ const postSurvey = (user) => {
 
   return { question:"Select teams you think consisted of the same people.",
            answers: answers,
-           correctAnswer: correctAnswer
-         }
+           correctAnswer: correctAnswer }
 }
