728a0ecafc9bf80b3b24be3ded1ca20d26a0c703
Phoebe <phoebek@stanford.edu>
2018-07-17 22:55:05 -0700

    get tone for all messages in .data/chat

--- nlp.js
+++ nlp.js
@@ -1,18 +1,44 @@
 require('dotenv').config();
-
+const fs = require('fs')
 const readline = require('readline');
-const ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+const ToneAnalyzer = require('watson-developer-cloud/tone-analyzer/v3');
 
-const tone_analyzer = new ToneAnalyzerV3({
-  username: process.env.TONE_ANALYZER_USERNAME,
-  password: process.env.TONE_ANALYZER_PASSWORD,
-  version_date: process.env.TONE_ANALYZER_VERSION_DATE
+const tone_analyzer = new ToneAnalyzer({
+  // TODO: figure out why accessing env file isn't working
+  // username: process.env.TONE_ANALYZER_USERNAME,
+  // password: process.env.TONE_ANALYZER_PASSWORD,
+  // version_date: process.env.TONE_ANALYZER_VERSION_DATE
+
+  //phoebe's ibm tone analyzer credentials - trial version
+  username: '1652b902-ddf2-4828-a54e-22c91ba9fcf9',
+  password: 'N3WpdlogdyOP',
+  version_date: '2017-09-21'
 });
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+const chatlogFile = '.data/chats'// should change this to access copy of db chats?
+let chatlogArr =[]
+fs.readFileSync(chatlogFile).toString().split('\n').forEach(function (line) {
+  chatlogArr.push(JSON.parse(line));
+})
+
+let chatlogText = chatlogArr.map(function(chatObj){
+    return chatObj.message;
+  }).join('\n');
+
+  let params = {
+    tone_input: chatlogText,
+    content_type: 'text/plain',
+    sentences: true
+  };
+
+  tone_analyzer.tone(params, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.stringify(response, null, 2));//should store this in db tone file
+    }
+  });
+
 
 // Setting up DB
 // const Datastore = require('nedb'),
@@ -24,24 +50,6 @@ const rl = readline.createInterface({
 //     db.midSurvey = new Datastore({ filename:'.data/midSurvey', autoload: true});
 
 
-
-rl.question('Please enter a short paragraph for Watson to analyze...', (text) => {
-
-  let params = {
-    tone_input: text,
-    content_type: 'text/plain',
-    sentences: true
-  };
-  tone_analyzer.tone(params, function (error, response) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.stringify(response, null, 2));//should store this in db tone file
-    }
-  });
-
-  rl.close();
-});
 
 
 
