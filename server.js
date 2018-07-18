b8156763e86aae8adbb600cba282935325337632
Nik Marda <nmarda@stanford.edu>
2018-07-18 14:43:49 -0700

    teamsize 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Settings - change for actual deployment
-const teamSize = 1
-const roundMinutes = .1
+const teamSize = 2
+const roundMinutes = .15
 
 // Toggles
 const autocompleteTestOn = 1 //turns on fake team to test autocomplete
