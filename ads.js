cfd01bec9e3f7e4d5db8436bbf8c011677e3d221
Mark Whiting <mark.whiting@gmail.com>
2018-07-25 06:58:02 -0700

    Set up child_process

--- ads.js
+++ ads.js
@@ -1,6 +1,15 @@
+const spawn = require("child_process").spawn;
 
+
+// more here: https://nodejs.org/api/child_process.html#child_process_subprocess_stdout
+const py = (func="", args="") => {
+  const pythonProcess = spawn('python',["ads.py",func, args]);
+  // pythonProcess.stdout.on('data', data => {console.log(data.toString('utf8'))} );
+  return pythonProcess.stdout.on('data')})
+
+console.log(["hi","bye"].map(py))
 
 module.exports = {
-  getBalance: getBalance,
-  launchBang: launchBang
+  getBalance: "THIS",
+  launchBang: "That"
 };
---
+++ ads.py
@@ -0,0 +1,5 @@
+import sys
+
+print("THIS IS WORKING")
+print(sys.argv[1])
+sys.stdout.flush()
