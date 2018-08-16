cbd98884f02e1a11828b221dfa4c10d7a8d77733
anhhuyalex <alexn@minerva.kgi.edu>
2018-08-15 17:57:21 -0700

    schedule bang every hour

--- scheduleBang.js
+++ scheduleBang.js
@@ -15,16 +15,17 @@ console.log(runningLive ? "\n RUNNING LIVE ".red.inverse : "\n RUNNING SANDBOXED
 
 //Reference HIT file
 let questionHTML = fs.readFileSync('./question.html').toString()
+let recruitingHITstorage = './txt/currentrecruitingHIT.txt'
 
 // Determine the lifetime of HIT
-const runtimeString = process.argv.length > 2 ? process.argv[2] : "" //if we specify a flag
-const actual_runTIME = new Date(runtimeString || "August 15 2018 11:00");
-const expireHITTime = new Date(actual_runTIME.getTime() - 30*60000); //get time 30 minutes before actualruntime
-const timeDiff = Math.abs(expireHITTime.getTime() - Date.now());
-const lifetime = timeDiff / 1000 / 60 //calculate lifetime based on when runTime was
+// const runtimeString = process.argv.length > 2 ? process.argv[2] : "" //if we specify a flag
+// const actual_runTIME = new Date(runtimeString || "August 15 2018 11:00");
+// const expireHITTime = new Date(actual_runTIME.getTime() - 30*60000); //get time 30 minutes before actualruntime
+// const timeDiff = Math.abs(expireHITTime.getTime() - Date.now());
+const lifetime = 60 //calculate lifetime based on when runTime was
 
 //Set HIT Params
-const title = `Get notified when our ad writing task launches. The next task will be at ${actual_runTIME.getHours()}:${actual_runTIME.getMinutes()} PST (California time) for up to 2 hours`
+const title = `Get notified when our ad writing task launches. The next task will be tomorrow morning PST (California time) for up to 2 hours`
 const description = "Submit this HIT to be notified when our ad writing task launches. Space is limited in our ad writing task but we will run many iterations of it. If you submit this HIT you will receive notifications about our HITs until you have completed that task."
 const assignmentDuration = 20
 const reward = 0.01
@@ -33,14 +34,28 @@ const keywords = "ad writing, qualification, future task"
 const maxAssignments = 200
 const taskURL = questionHTML
 
-// let HITId = "3PCPFX4U405XHZWRW7GXXH3U9U5FQT"
+// Assign willBang to people who have accepted recruiting HIT of last hour
+if (fs.readFileSync('').toString()) {
+  let HITId = fs.readFileSync(recruitingHITstorage).toString();
+  mturk.listAssignments(HITId, data => {
+    mturk.assignQualificationToUsers(data.Assignments.map(a => a.WorkerId), mturk.quals.willBang)
+  })
+}
 
-//Make HIT
+// Delete recruitingHITstorage
+fs.unlink(recruitingHITstorage, (err) => {
+  if (err) throw err;
+  console.log('recruitingHITstorage was deleted');
+});
+
+
+//Make new recruiting HIT
 mturk.makeHIT(title, description, assignmentDuration, lifetime, reward, autoApprovalDelay, keywords, maxAssignments, taskURL, (HIT) => {
   const HITId = HIT.HITId;
-  db.ourHITs.insert({'currentHIT': HITId}, (err, HITAdded) => {
-    if(err) console.log("There's a problem adding HIT to the DB: ", err);
-    else if(HITAdded) console.log("HIT added to the DB: ", HITId);
+  // Write new recruiting HIT id to file for next hour run
+  fs.writeFile(recruitingHITstorage, HITId, (err, HITAdded) => {
+    if(err) console.log("There's a problem writing HIT to the recruiting file: ", err);
+    else if(HITAdded) console.log("HIT added to the recruiting file: ", HITId);
   });
 })
 
