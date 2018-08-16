2e55419b02fa1dcc77f7ac0df4e14d7ef88fad17
anhhuyalex <alexn@minerva.kgi.edu>
2018-08-16 11:27:58 -0700

    change fs.writefile callback

--- scheduleBang.js
+++ scheduleBang.js
@@ -53,9 +53,8 @@ fs.unlink(recruitingHITstorage, (err) => {
 mturk.makeHIT(title, description, assignmentDuration, lifetime, reward, autoApprovalDelay, keywords, maxAssignments, taskURL, (HIT) => {
   const HITId = HIT.HITId;
   // Write new recruiting HIT id to file for next hour run
-  fs.writeFile(recruitingHITstorage, HITId, (err, HITAdded) => {
+  fs.writeFile(recruitingHITstorage, HITId, (err) => {
     if(err) console.log("There's a problem writing HIT to the recruiting file: ", err);
-    else if(HITAdded) console.log("HIT added to the recruiting file: ", HITId);
   });
 })
 
