62358649e5cfd0bd519b4ec4b7105d7419fc4293
anhhuyalex <alexn@minerva.kgi.edu>
2018-08-24 01:52:25 -0700

    notify only if we need to

--- server.js
+++ server.js
@@ -234,14 +234,16 @@ if (runExperimentNow){ mturk.launchBang(function(HIT) {
           if (err) {console.log("DB for MTurk:" + err)}
           else { //if we don't have enough people with current time preference to notify 
             let moreworkersneeded = maxWorkersToNotify - currentTimePoolWorkers.length
-            db.willBang.find({ timePreference: '' }, (err, workersfromnullPool) => {
-              if (err) {console.log("DB for MTurk:" + err)}
-              else {
-                workersfromnullPool = getRandomSubarray(workersfromnullPool, moreworkersneeded)
-                let workerstonotify = currentTimePoolWorkers.concat(workersfromnullPool).map(u => u.id)
-                mturk.notifyWorkers(workerstonotify, subject, message)
-              }
-            })
+            if (moreworkersneeded > 0) {
+              db.willBang.find({ timePreference: '' }, (err, workersfromnullPool) => {
+                if (err) {console.log("DB for MTurk:" + err)}
+                else {
+                  workersfromnullPool = getRandomSubarray(workersfromnullPool, moreworkersneeded)
+                  let workerstonotify = currentTimePoolWorkers.concat(workersfromnullPool).map(u => u.id)
+                  mturk.notifyWorkers(workerstonotify, subject, message)
+                }
+              })
+            }
           }
         })
 
