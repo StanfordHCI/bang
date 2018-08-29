025de38aba15c954c30cf25682dcce452b26d047
allie blaising <allieblaising@gmail.com>
2018-08-28 18:15:28 -0700

    r stuff

--- bangData.js
+++ bangData.js
@@ -191,8 +191,8 @@ function downloadData(url,callback) {
 }
 
 //Save from servers
-/* downloadData("mark.dmorina.com",saveAllData) */
-/* downloadData("bang.dmorina.com",saveAllData) */
+ downloadData("mark.dmorina.com",saveAllData) 
+ // downloadData("bang.dmorina.com",saveAllData) 
 
 //Save from local folder
 // saveAllData()
