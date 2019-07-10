afde57bb29ab3bd7a7041cc44993b2fbb90a1c67
madewithsmiles <jndiarrassouba@gmail.com>
2019-07-10 00:54:38 -0700

    Added url parsing functionality and the possibility to type. Still needed: allow users which are not logged in to still unsubscribe. Communicate with the backend. Test!

--- front/src/containers/Unsubscribe.js
+++ front/src/containers/Unsubscribe.js
@@ -16,18 +16,26 @@ class Unsubscribe extends Component {
     this.state = {
       parsedUrl: "",
       mTurkId: "",
-      error: ""
-      // isReady: false,
+      error: "",
+      isReady: false
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentWillMount() {
-    // Promise.all([
-    //   this.props.loadTemplate(this.props.match.params.id),
-    // ])
-    //   .then(()=>{
-    //     this.setState({isReady: true})
-    //   })
+    // Parse from the link if there is no mTurkId in the box 
+    // and there is a value in a link
+    this.setState({
+      isReady: true,
+      mTurkId:
+        this.state.mTurkId == "" && this.props.match.params.id
+          ? this.props.match.params.id
+          : this.state.mTurkId
+    });
+  }
+
+  handleChange(event) {
+    this.setState({ mTurkId: event.target.value });
   }
 
   render() {
@@ -36,47 +44,46 @@ class Unsubscribe extends Component {
         <Row>
           <Col md={12} lg={12} xl={12}>
             <Card>
-              {/* {
-            this.state.isReady &&  */}
-              <CardBody>
-                <div className="card__title">
-                  <h5 className="bold-text">Unsubscribe</h5>
-                </div>
-                <div>
-                    Please, contact us if you have already completed the study and still receive the emails. You will not be able to unsubscribe through this page.<br /> 
-                    Otherwise, if you were in a waiting room at some point and never got to complete the HIT, you may use this page to unsubscribe.<br />
-                    If successful, will allow you to complete the HIT.<br /><br />
-                </div>
-                <div
-                  style={{
-                    // justifyContent: "center",
-                    // alignItems: "center",
-                    // padding: "20px"
-                  }}
-                >
-                  <input
-                    style={{ minWidth: "100%" }}
-                    placeholder="Your mTurkId"
-                  />
-                  <Button
-                    style={{ display: "flex", minWidth: "10%", marginTop: "20px" }}
-                    onClick={() => alert("You clicked on Join Batch")}
-                    className="btn btn-primary"
-                  >
-                    Unsubscribe
-                  </Button>
-                </div>
-              </CardBody>
-              {/* div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "20px"
-                }} 
-              </div>
-              {/* } */}
+              {this.state.isReady && (
+                <CardBody>
+                  <div className="card__title">
+                    <h5 className="bold-text">Unsubscribe</h5>
+                  </div>
+                  <div>
+                    Please, contact us if you have <b>already</b> completed the
+                    study <b>but</b> still receive emails. You will not be able
+                    to unsubscribe through this page.
+                    <br />
+                    Otherwise, if you were in a <b>waiting room</b> at some
+                    point <b>and never got to complete the HIT</b>, you may use
+                    this page to unsubscribe.
+                    <br />
+                    Upon unsubscription, you will still be allowed to complete the HIT.
+                    <br />
+                    <br />
+                  </div>
+                  <div>
+                    <input
+                      style={{ minWidth: "100%" }}
+                      autoFocus
+                      value={this.state.mTurkId ? this.state.mTurkId : ""}
+                      placeholder={"Enter your mTurkId here"}
+                      onChange={this.handleChange}
+                    />
+                    <Button
+                      style={{
+                        display: "flex",
+                        minWidth: "10%",
+                        marginTop: "20px"
+                      }}
+                      onClick={() => alert(`You wanna unsubscribe mTurkId = ${this.state.mTurkId}`)}
+                      className="btn btn-primary"
+                    >
+                      Unsubscribe
+                    </Button>
+                  </div>
+                </CardBody>
+              )}
             </Card>
           </Col>
         </Row>
