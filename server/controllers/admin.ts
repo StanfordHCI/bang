d8391d8cc7730e77a7c5375ad84fd0cbea25a9fd
Michelle Xing <MiXing@Mac-4.local>
2019-07-18 14:57:27 -0700

    fixed concatenation of string, parts of email was cut out before

--- server/controllers/admin.ts
+++ server/controllers/admin.ts
@@ -109,8 +109,8 @@ export const addBatch = async function (req, res) {
         const message = 'Hi! Our HIT is now active. We are starting a new experiment on Bang. ' + 
           'Your FULL participation will earn you a bonus of ~$12/hour. ' + '\n\n' +
           'Please join the HIT here: ' + url + '\n\n' +
-          'The link will bring you to click the JOIN BATCH button which will allow you to enter the WAITING ROOM ' + 
-          'NOTE: You will be bonused $1 if enough users join the waiting room and the task starts' + '\n\n'
+          'The link will bring you to click the JOIN BATCH button which will allow you to enter the WAITING ROOM. ' + 
+          'NOTE: You will be bonused $1 if enough users join the waiting room and the task starts' + '\n\n' + 
           'Our records indicate that you were interested in joining this HIT previously. ' +
           'If you are no longer interested in participating, please UNSUBSCRIBE here: ' + unsubscribe_url;
         notifyWorkers([user.mturkId], message, 'Bang')
@@ -267,12 +267,12 @@ export const notifyUsers = async function (req, res) {
           const url = process.env.HIT_URL + '?assignmentId=' + user.testAssignmentId + '&workerId=' + user.mturkId;
           const unsubscribe_url = process.env.HIT_URL + 'unsubscribe/' + user.mturkId; 
           const message = 'Hi! Our HIT is now active. We are starting a new experiment on Bang. ' + 
-            'Your FULL participation will earn you a bonus of ~$12/hour. ' + '\n\n' +
-            'Please join the HIT here: ' + url + '\n\n' +
-            'The link will bring you to click the JOIN BATCH button which will allow you to enter the WAITING ROOM ' + 
-            'NOTE: You will be bonused $1 if enough users join the waiting room and the task starts' + '\n\n'
-            'Our records indicate that you were interested in joining this HIT previously. ' +
-            'If you are no longer interested in participating, please UNSUBSCRIBE here: ' + unsubscribe_url;
+          'Your FULL participation will earn you a bonus of ~$12/hour. ' + '\n\n' +
+          'Please join the HIT here: ' + url + '\n\n' +
+          'The link will bring you to click the JOIN BATCH button which will allow you to enter the WAITING ROOM. ' + 
+          'NOTE: You will be bonused $1 if enough users join the waiting room and the task starts' + '\n\n' + 
+          'Our records indicate that you were interested in joining this HIT previously. ' +
+          'If you are no longer interested in participating, please UNSUBSCRIBE here: ' + unsubscribe_url;
           notifyWorkers([user.mturkId], message, 'Bang')
             .then(() => {
               counter++;
