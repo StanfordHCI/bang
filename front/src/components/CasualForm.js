39910140aaa0e6947aa8d43cdcdce42b4a941b80
deliveryweb <dev@deliveryweb.ru>
2020-01-22 01:00:01 +0800

    Added support for new question types

--- front/src/components/CasualForm.js
+++ front/src/components/CasualForm.js
@@ -2,28 +2,33 @@ import React from "react";
 import {change, Field, formValueSelector, reduxForm} from "redux-form";
 import {Button, ButtonToolbar, Col, Container, Row} from "reactstrap";
 import renderRadioPanel from "Components/form/RadioPanel";
+import renderRadioButton from "Components/form/RadioButton";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import {Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+import {renderTextArea} from 'Components/form/Text';
 
-const renderRadioButtonField1 = (props) => {
-    return <Label check>
-        <Input type="radio" name="radio1"/>
-        {props.label}
-    </Label>
-};
-
-const renderOptionsQuestion = ({options, index}) => {
+const renderRadioButtonField = (props) => {
     return (
-                <Field
-                    name='result'
-                    component={renderRadioPanel}
-                    options={options}
-                />
-        )
-
-
+        <FormGroup check>
+            <Label check>
+                <Input {...props.input} name={props.input.name} type="radio"/>{' '}
+                {props.option.label}
+            </Label>
+        </FormGroup>
+    )
 };
+
+const renderCheckBoxField = (props) => {
+    return (
+        <FormGroup check>
+            <Label check>
+                <Input {...props.input} type="checkbox"/>{' '}
+                {props.option.label}
+            </Label>
+        </FormGroup>
+    )
+}
 
 class CasualForm extends React.Component {
 
@@ -35,43 +40,65 @@ class CasualForm extends React.Component {
     render() {
         const {questions} = this.props;
         return (
-            <form className='form form--horizontal'
-                  onSubmit={this.props.handleSubmit}>
-                <Container>
-                    <Row>
-                        <p style={{color: 'black'}}>Questions</p>
-                        {
-                            questions.map(question => (
-                                question.type === 2 &&
-                                <Row>
-                                    <Col className='form__form-group'>
-                                        <label className='form__form-group-label'
-                                               style={{maxWidth: '50px', textAlign: 'center'}}><p style={{
-                                            color: 'grey',
-                                            textAlign: 'center',
-                                            lineHeight: '180%'
-                                        }}>{question.text}</p></label>
+            <form onSubmit={this.props.handleSubmit}>
+                <FormGroup>
+                    <p style={{color: 'black'}}>Questions</p>
+                    {
+                        questions.map((question, index) => {
+                            if (question.type === 2) {
+                                return <FormGroup tag="fieldset">
+                                    <Label>{question.text}</Label>
+                                    <FormGroup check>
+                                        {
+                                            question.selectOptions.map(option => (
+                                                <Field
+                                                    name={`questions.${index}.result`}
+                                                    component={renderRadioButtonField}
+                                                    type="radio"
+                                                    option={option}
+                                                    value={option.value}
+                                                />
+                                            ))
+                                        }
+                                    </FormGroup>
+                                </FormGroup>
+                            }
+                            if (question.type === 3) {
+                                return <FormGroup tag="fieldset">
+                                    <Label>{question.text}</Label>
+                                    <FormGroup check>
+                                        {
                                             <Field
-                                                name={`${question}.options`}
-                                                component={renderOptionsQuestion}
-                                                rerenderOnEveryChange
-                                                withPoints={false}
-                                                options={question.selectOptions}
+                                                name={`questions.${index}.result`}
+                                                component="input"
+                                                type="text"
                                             />
-                                    </Col>
-                                </Row>
-                            ))
-                        }
-                    </Row>
-                    <Row>
-                        <Col>
-                            <ButtonToolbar className='mx-auto form__button-toolbar'>
-                                <Button color='primary' size='sm' type='submit'
-                                >Submit</Button>
-                            </ButtonToolbar>
-                        </Col>
-                    </Row>
-                </Container>
+                                        }
+                                    </FormGroup>
+                                </FormGroup>
+                            }
+                            if (question.type === 4) {
+                                return <FormGroup tag="fieldset">
+                                    <Label>{question.text}</Label>
+                                    <FormGroup check>
+                                        {
+                                            question.selectOptions.map((option, optionIndex) => (
+                                                <Field
+                                                    name={`questions.${index}.result.${optionIndex}`}
+                                                    component={renderCheckBoxField}
+                                                    option={option}
+                                                    value={option.value}
+                                                />
+                                            ))
+                                        }
+                                    </FormGroup>
+                                </FormGroup>
+                            }
+                        })
+                    }
+                </FormGroup>
+                <Button color='primary' size='sm' type='submit'
+                >Submit</Button>
             </form>
         )
     }
@@ -89,20 +116,9 @@ const validate = (values, props) => {
 };
 
 CasualForm = reduxForm({
-    form: 'TemplateForm',
-    enableReinitialize: true,
-    touchOnChange: true,
+    form: 'CasualForm',
     validate,
 })(CasualForm);
 
-const selector = formValueSelector('CasualForm');
 
-function mapStateToProps(state) {
-    return {}
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({}, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CasualForm);
+export default CasualForm;
