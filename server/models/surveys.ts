ca41348965832b83ce009de89499524c0f2b85e1
danonya666 <tatarenkov16d@gmail.com>
2020-02-11 19:32:09 +0300

    codestyle

--- server/models/surveys.ts
+++ server/models/surveys.ts
@@ -1,39 +1,50 @@
 const options = {
-	timestamps: true,
-	toObject: {
-		getters: true,
-		virtuals: true
-	}
+    timestamps: true,
+    toObject: {
+        getters: true,
+        virtuals: true
+    }
 };
 import * as mongoose from 'mongoose';
+
 const Schema = mongoose.Schema;
 let SurveySchema = new Schema(
-	{
-		user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-		batch: { type: mongoose.Schema.Types.ObjectId, ref: 'Batch' },
-		questions: [ { result: { type: String, required: false }, type: {type:String, enum: ['primary', 'text', 'single', 'checkbox']}, result_array: [{type: Number}] , number: Number, teammate: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } } ],
-		mainQuestion: {
-			//Picking Rounds
-			expRound1: Number,
-			expRound2: Number,
-			//Why these rounds?
-			expRound3: String,
-			//Partner preferences
-			partners: [ { type: mongoose.Schema.Types.ObjectId, ref: 'User' } ],
-			//Why this partner?
-			partners2: String
-		},
-		singleTeamQuestion: {
-			chosenPartnerName: String,
-			actualPartnerName: String,
-			result1: String,
-			numOptions: Number,
-		},
-		round: Number,
-		isPost: Boolean, //deprecated, used only to show old results
-    surveyType: { type: String, enum: ['presurvey', 'midsurvey', 'postsurvey', 'final', 'poll', 'prepresurvey'], required: true }, //should be used instead of isPost
-		pollInd: { type: Number, required: false, default: 0 }, // for poll type surveys only
-	},
-	options
+    {
+        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        batch: {type: mongoose.Schema.Types.ObjectId, ref: 'Batch'},
+        questions: [{
+            result: {type: String, required: false},
+            type: {type: String, enum: ['primary', 'text', 'single', 'checkbox']},
+            result_array: [{type: Number}],
+            number: Number,
+            teammate: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+        }],
+        mainQuestion: {
+            //Picking Rounds
+            expRound1: Number,
+            expRound2: Number,
+            //Why these rounds?
+            expRound3: String,
+            //Partner preferences
+            partners: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+            //Why this partner?
+            partners2: String
+        },
+        singleTeamQuestion: {
+            chosenPartnerName: String,
+            actualPartnerName: String,
+            result1: String,
+            numOptions: Number,
+        },
+        round: Number,
+        isPost: Boolean, //deprecated, used only to show old results
+        surveyType: {
+            type: String,
+            enum: ['presurvey', 'midsurvey', 'postsurvey', 'final', 'poll', 'prepresurvey'],
+            required: true
+        }, //should be used instead of isPost
+        pollInd: {type: Number, required: false, default: 0}, // for poll type surveys only
+    },
+    options
 );
 export const Survey = mongoose.model('Survey', SurveySchema);
