b0b6e4e8c8ff7aaac241e8b1102d5e26c53cdec1
Phoebe Yao <phoebeyao2@gmail.com>
2020-02-10 18:57:40 -0800

    initial attempt at editing survey JSON

--- front/src/containers/RoundSurveyForm.js
+++ front/src/containers/RoundSurveyForm.js
@@ -1,41 +1,80 @@
 /** midsurveyform.js
  *  front-end
- * 
+ *
  *  team viability questionnaire in middle
- *  
- *  renders:  
+ *
+ *  renders:
  *    1. during worker's mid-surveys (not read-only)
  *    2. when viewing results (as read-only)
- * 
+ *
  *  called by:
  *    1. Batch.js
  *    2. BatchResult.js
  */
 
+import React from "react";
+import { Col, Button, ButtonToolbar, Row, Container } from "reactstrap";
+import { connect } from "react-redux";
+import {
+  Field,
+  FieldArray,
+  reduxForm,
+  formValueSelector,
+  change
+} from "redux-form";
+import { bindActionCreators } from "redux";
+import { renderField } from "Components/form/Text";
+import renderRadioPanel from "Components/form/RadioPanel";
+import { renderTextArea } from "../components/form/Text";
+import { shuffle } from "../utils";
 
-import React from 'react';
-import {Col, Button, ButtonToolbar, Row, Container} from 'reactstrap';
-import {connect} from 'react-redux'
-import {Field, FieldArray, reduxForm, formValueSelector, change} from 'redux-form'
-import {bindActionCreators} from "redux";
-import {renderField} from 'Components/form/Text'
-import renderRadioPanel from 'Components/form/RadioPanel'
-import {renderTextArea} from "../components/form/Text";
-import {shuffle} from "../utils";
+const replaceNicksInSurvey = (
+  message,
+  users,
+  currentUser,
+  readOnly,
+  unmasked,
+  surveyType
+) => {
+  names = [];
+  if (readOnly || surveyType === "pre") return message;
+  users
+    .filter(user => currentUser._id.toString() !== user._id.toString())
+    .forEach((user, index) => {
+      newMessage = message.replace(
+        new RegExp("team_partner_" + (index + 1), "ig"),
+        unmasked ? user.realNick : user.fakeNick
+      );
+      if (newMessage !== message) {
+        names.append(user._id.toString());
+        message = newMessage;
+      }
+    });
+  newMessage = message.replace(
+    new RegExp("user_own_name", "ig"),
+    currentUser.realNick
+  );
+  if (newMessage !== message) {
+    names.append(currentUser._id.toString());
+    message = newMessage;
+  }
+  messageContainer = [message, names];
+  return messageContainer;
+};
 
-const replaceNicksInSurvey = (message, users, currentUser, readOnly, unmasked, surveyType) => {
-  if (readOnly || surveyType === 'pre') return message;
-  users.filter(user => currentUser._id.toString() !== user._id.toString()).forEach((user, index) => {
-    message = message.replace(new RegExp('team_partner_' + (index + 1), "ig"), unmasked ? user.realNick : user.fakeNick)
-  })
-  message = message.replace(new RegExp('user_own_name', "ig"), currentUser.realNick)
-  return message;
-}
-
-
-const renderQuestions = ({fields, meta: {touched, error, warning}, questions, readOnly, users, currentUser, unmasked, surveyType, team}) => {
+const renderQuestions = ({
+  fields,
+  meta: { touched, error, warning },
+  questions,
+  readOnly,
+  users,
+  currentUser,
+  unmasked,
+  surveyType,
+  team
+}) => {
   let items = [];
-  let itemsWithIndexes = []
+  let itemsWithIndexes = [];
   for (let i = 0; i < questions.length; i++) {
     /*let hasVarOfAfkUser = false, selectOptions = [];
     if (questions[i].type ==='select') {
@@ -51,79 +90,128 @@ const renderQuestions = ({fields, meta: {touched, error, warning}, questions, re
       }
     })*/
     const orderedQuestions = questions;
-    const component = orderedQuestions[i].type === 'select' ? renderRadioPanel : (orderedQuestions[i].type === 'text' ? renderField : renderTextArea);
+    const component =
+      orderedQuestions[i].type === "select"
+        ? renderRadioPanel
+        : orderedQuestions[i].type === "text"
+        ? renderField
+        : renderTextArea;
+    const messageContainer = replaceNicksInSurvey(
+      orderedQuestions[i].question,
+      users,
+      currentUser,
+      readOnly,
+      unmasked,
+      surveyType
+    );
+
     items.push(
-      <div key={i} className='form__form-group'>
-        <label className='form__form-group-label'>
-            {replaceNicksInSurvey(orderedQuestions[i].question, users, currentUser, readOnly, unmasked, surveyType)}
-          </label>
-        <div className='form__form-group-field' style={{maxWidth: '700px'}}>
-          {orderedQuestions[i].type !== 'instruction' && <Field
-            name={`questions[${i}].result`}
-            component={component}
-            type={orderedQuestions[i].type}
-            disabled={readOnly}
-            options={orderedQuestions[i].type ==='select' ? orderedQuestions[i].selectOptions.map(x => {return {label: replaceNicksInSurvey(x.label, users, currentUser, readOnly, unmasked, surveyType), value: x.value}}) : []}
-          />}
-          {orderedQuestions[i].type === 'instruction' && <label>
-            {orderedQuestions[i].question}
-          </label>}
+      <div key={i} className="form__form-group">
+        <label className="form__form-group-label">{messageContainer[0]}</label>
+        <div className="form__form-group-field" style={{ maxWidth: "700px" }}>
+          {orderedQuestions[i].type !== "instruction" && (
+            <Field
+              name={`questions[${i}].result`}
+              component={component}
+              type={orderedQuestions[i].type}
+              disabled={readOnly}
+              options={
+                orderedQuestions[i].type === "select"
+                  ? orderedQuestions[i].selectOptions.map(x => {
+                      return {
+                        label: replaceNicksInSurvey(
+                          x.label,
+                          users,
+                          currentUser,
+                          readOnly,
+                          unmasked,
+                          surveyType
+                        )[0],
+                        value: x.value
+                      };
+                    })
+                  : []
+              }
+            />
+          )}
+          {orderedQuestions[i].type === "instruction" && (
+            <label>{orderedQuestions[i].question}</label>
+          )}
         </div>
       </div>
     );
-    itemsWithIndexes[i] = {item: items[i], index: orderedQuestions[i].fakeIndex}
+    itemsWithIndexes[i] = {
+      item: items[i],
+      index: orderedQuestions[i].fakeIndex
+    };
   }
   items = itemsWithIndexes.sort((a, b) => a.index - b.index).map(c => c.item);
 
-  return (<div style={{marginTop: '20px'}}>
-    {items}
-  </div>)
+  return <div style={{ marginTop: "20px" }}>{items}</div>;
 };
 
 class RoundSurveyForm extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      fakeIndexed: [{
-        question: 'a',
-        type: 'select',
-        options: [
-          {option: 'Strongly Disagree'},
-          {option: 'Disagree', },
-          {option: 'Neutral', },
-          {option: 'Agree', },
-          {option: 'Strongly Agree'},
-        ],
-        selectOptions: [
-          {label: 'Strongly Disagree', value: `0`},
-          {label: 'Disagree', value: `1`},
-          {label: 'Neutral', value: `2`},
-          {label: 'Agree', value: `3`},
-          {label: 'Strongly Agree', value: `4`},
-        ],
-        fakeIndex: 0
-      }],
+      fakeIndexed: [
+        {
+          question: "a",
+          type: "select",
+          options: [
+            { option: "Strongly Disagree" },
+            { option: "Disagree" },
+            { option: "Neutral" },
+            { option: "Agree" },
+            { option: "Strongly Agree" }
+          ],
+          selectOptions: [
+            { label: "Strongly Disagree", value: `0` },
+            { label: "Disagree", value: `1` },
+            { label: "Neutral", value: `2` },
+            { label: "Agree", value: `3` },
+            { label: "Strongly Agree", value: `4` }
+          ],
+          fakeIndex: 0
+        }
+      ]
     };
   }
 
   componentDidMount() {
-    const {questions} = this.props;
+    const { questions } = this.props;
     if (questions.some(x => x.randomOrder)) {
       // puts randomOrder questions into random order after non-randomOrder questions
       let tempIndex = 0;
-      questions.filter(x => !x.randomOrder).forEach(x => x.fakeIndex = tempIndex++);
+      questions
+        .filter(x => !x.randomOrder)
+        .forEach(x => (x.fakeIndex = tempIndex++));
       if (questions.some(x => x.randomOrder)) {
-        const indexes = Array.from(questions.filter(x => x.randomOrder).keys()).map(x => x + tempIndex);
+        const indexes = Array.from(
+          questions.filter(x => x.randomOrder).keys()
+        ).map(x => x + tempIndex);
         const shuffled = shuffle(indexes);
-        questions.filter(x => x.randomOrder).forEach((x, ind) => x.fakeIndex = shuffled[ind]);
+        questions
+          .filter(x => x.randomOrder)
+          .forEach((x, ind) => (x.fakeIndex = shuffled[ind]));
       }
     }
-    this.setState({fakeIndexed: questions});
+    this.setState({ fakeIndexed: questions });
   }
 
   render() {
-    const {invalid, questions, readOnly, currentUser, members, batch, surveyType, team, selectiveMasking, user} = this.props;
+    const {
+      invalid,
+      questions,
+      readOnly,
+      currentUser,
+      members,
+      batch,
+      surveyType,
+      team,
+      selectiveMasking,
+      user
+    } = this.props;
     let newQuestions = [...questions];
     if (selectiveMasking) {
       console.log(team.users);
@@ -131,34 +219,51 @@ class RoundSurveyForm extends React.Component {
         if (x.user !== user._id) {
           newQuestions.push({
             question: `${x.nickname} is someone I would like to work with again.`,
-            type: 'select',
+            type: "select",
             options: [
-                {option: 'Strongly Disagree'},
-                {option: 'Disagree', },
-                {option: 'Neutral', },
-                {option: 'Agree', },
-                {option: 'Strongly Agree'},
-              ],
-            selectOptions: [
-                {label: 'Strongly Disagree', value: `0 ${x.user}`},
-                {label: 'Disagree', value: `1 ${x.user}`},
-                {label: 'Neutral', value: `2 ${x.user}`},
-                {label: 'Agree', value: `3 ${x.user}`},
-                {label: 'Strongly Agree', value: `4 ${x.user}`},
+              { option: "Strongly Disagree" },
+              { option: "Disagree" },
+              { option: "Neutral" },
+              { option: "Agree" },
+              { option: "Strongly Agree" }
             ],
-          })
-          console.log(`pushed to newQuestions. ${newQuestions}`)
+            selectOptions: [
+              { label: "Strongly Disagree", value: `0 ${x.user}` },
+              { label: "Disagree", value: `1 ${x.user}` },
+              { label: "Neutral", value: `2 ${x.user}` },
+              { label: "Agree", value: `3 ${x.user}` },
+              { label: "Strongly Agree", value: `4 ${x.user}` }
+            ]
+          });
+          console.log(`pushed to newQuestions. ${newQuestions}`);
         }
-      })
+      });
     }
-    return (<div style={{width: '100%'}}>
-      {!readOnly && <p> IMPORTANT: Finishing the survey is <b>required</b> to participate in this experiment.</p>}
-      {!readOnly && <p> If you do not finish the survey, <b>you will NOT be paid for this task.</b> </p>}
-        <form className='form' style={{paddingBottom: '5vh'}} onSubmit={this.props.handleSubmit}>
+    return (
+      <div style={{ width: "100%" }}>
+        {!readOnly && (
+          <p>
+            {" "}
+            IMPORTANT: Finishing the survey is <b>required</b> to participate in
+            this experiment.
+          </p>
+        )}
+        {!readOnly && (
+          <p>
+            {" "}
+            If you do not finish the survey,{" "}
+            <b>you will NOT be paid for this task.</b>{" "}
+          </p>
+        )}
+        <form
+          className="form"
+          style={{ paddingBottom: "5vh" }}
+          onSubmit={this.props.handleSubmit}
+        >
           <Container>
             <Row>
               <Col>
-                <div className='form__panel'>
+                <div className="form__panel">
                   <FieldArray
                     name="questions"
                     component={renderQuestions}
@@ -167,70 +272,85 @@ class RoundSurveyForm extends React.Component {
                     readOnly={readOnly}
                     users={members}
                     currentUser={currentUser}
-                    unmasked={batch ? batch.maskType === 'unmasked' : 'masked'}
+                    unmasked={batch ? batch.maskType === "unmasked" : "masked"}
                     surveyType={surveyType}
                     team={team}
                   />
-              </div>
+                </div>
               </Col>
             </Row>
-            {!readOnly && <Row>
-              <Col>
-                <ButtonToolbar className='mx-auto form__button-toolbar'>
-                  <Button color='primary' size='sm' type='submit' disabled={invalid}>Submit</Button>
-                </ButtonToolbar>
-              </Col>
-            </Row>}
+            {!readOnly && (
+              <Row>
+                <Col>
+                  <ButtonToolbar className="mx-auto form__button-toolbar">
+                    <Button
+                      color="primary"
+                      size="sm"
+                      type="submit"
+                      disabled={invalid}
+                    >
+                      Submit
+                    </Button>
+                  </ButtonToolbar>
+                </Col>
+              </Row>
+            )}
           </Container>
         </form>
       </div>
-    )
+    );
   }
 }
 
 const validate = (values, props) => {
   const surveyType = props.surveyType;
   const questions = props.questions;
-  const errors = {questions: []};
-  if (values.questions) for (let i = 0; i < values.questions.length; i++) {
-    if (surveyType !== 'prepre') { // standard validation
-      if (!values.questions[i]) {
-        errors.questions[i] = {result: 'required'}
-      } else {
-        if (!values.questions[i].result && values.questions[i].result !== 0) {
-          errors.questions[i] = {result: 'required'}
-        }
-      }
-    } else { // we do not validate text questions in prepre surveys for the sake of demographics survey
-      if (questions[i].type !== 'text') {
+  const errors = { questions: [] };
+  if (values.questions)
+    for (let i = 0; i < values.questions.length; i++) {
+      if (surveyType !== "prepre") {
+        // standard validation
         if (!values.questions[i]) {
-          errors.questions[i] = {result: 'required'}
+          errors.questions[i] = { result: "required" };
         } else {
           if (!values.questions[i].result && values.questions[i].result !== 0) {
-            errors.questions[i] = {result: 'required'}
+            errors.questions[i] = { result: "required" };
+          }
+        }
+      } else {
+        // we do not validate text questions in prepre surveys for the sake of demographics survey
+        if (questions[i].type !== "text") {
+          if (!values.questions[i]) {
+            errors.questions[i] = { result: "required" };
+          } else {
+            if (
+              !values.questions[i].result &&
+              values.questions[i].result !== 0
+            ) {
+              errors.questions[i] = { result: "required" };
+            }
           }
         }
       }
     }
-  }
 
-  return errors
+  return errors;
 };
 
 RoundSurveyForm = reduxForm({
-  form: 'SurveyForm',
+  form: "SurveyForm",
   enableReinitialize: true,
   destroyOnUnmount: true,
-  validate,
+  validate
 })(RoundSurveyForm);
 
-const selector = formValueSelector('SurveyForm');
+const selector = formValueSelector("SurveyForm");
 
 function mapStateToProps(state) {
   return {
     currentUser: state.app.user,
     batch: state.batch.batch
-  }
+  };
 }
 
 function mapDispatchToProps(dispatch) {
