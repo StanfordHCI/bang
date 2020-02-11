1c6ed5eb6df8702eef38afac1699697b1bf71c99
danonya666 <tatarenkov16d@gmail.com>
2020-02-11 18:01:48 +0300

    codestyle

--- front/src/containers/RoundSurveyForm.js
+++ front/src/containers/RoundSurveyForm.js
@@ -1,12 +1,12 @@
 /** midsurveyform.js
  *  front-end
- * 
+ *
  *  team viability questionnaire in middle
- *  
- *  renders:  
+ *
+ *  renders:
  *    1. during worker's mid-surveys (not read-only)
  *    2. when viewing results (as read-only)
- * 
+ *
  *  called by:
  *    1. Batch.js
  *    2. BatchResult.js
@@ -55,15 +55,20 @@ const renderQuestions = ({fields, meta: {touched, error, warning}, questions, re
     items.push(
       <div key={i} className='form__form-group'>
         <label className='form__form-group-label'>
-            {replaceNicksInSurvey(orderedQuestions[i].question, users, currentUser, readOnly, unmasked, surveyType)}
-          </label>
+          {replaceNicksInSurvey(orderedQuestions[i].question, users, currentUser, readOnly, unmasked, surveyType)}
+        </label>
         <div className='form__form-group-field' style={{maxWidth: '700px'}}>
           {orderedQuestions[i].type !== 'instruction' && <Field
             name={`questions[${i}].result`}
             component={component}
             type={orderedQuestions[i].type}
             disabled={readOnly}
-            options={orderedQuestions[i].type ==='select' ? orderedQuestions[i].selectOptions.map(x => {return {label: replaceNicksInSurvey(x.label, users, currentUser, readOnly, unmasked, surveyType), value: x.value}}) : []}
+            options={orderedQuestions[i].type === 'select' ? orderedQuestions[i].selectOptions.map(x => {
+              return {
+                label: replaceNicksInSurvey(x.label, users, currentUser, readOnly, unmasked, surveyType),
+                value: x.value
+              }
+            }) : []}
           />}
           {orderedQuestions[i].type === 'instruction' && <label>
             {orderedQuestions[i].question}
@@ -90,9 +95,9 @@ class RoundSurveyForm extends React.Component {
         type: 'select',
         options: [
           {option: 'Strongly Disagree'},
-          {option: 'Disagree', },
-          {option: 'Neutral', },
-          {option: 'Agree', },
+          {option: 'Disagree',},
+          {option: 'Neutral',},
+          {option: 'Agree',},
           {option: 'Strongly Agree'},
         ],
         selectOptions: [
@@ -133,18 +138,18 @@ class RoundSurveyForm extends React.Component {
             question: `${x.nickname} is someone I would like to work with again.`,
             type: 'select',
             options: [
-                {option: 'Strongly Disagree'},
-                {option: 'Disagree', },
-                {option: 'Neutral', },
-                {option: 'Agree', },
-                {option: 'Strongly Agree'},
-              ],
+              {option: 'Strongly Disagree'},
+              {option: 'Disagree',},
+              {option: 'Neutral',},
+              {option: 'Agree',},
+              {option: 'Strongly Agree'},
+            ],
             selectOptions: [
-                {label: 'Strongly Disagree', value: `0 ${x.user}`},
-                {label: 'Disagree', value: `1 ${x.user}`},
-                {label: 'Neutral', value: `2 ${x.user}`},
-                {label: 'Agree', value: `3 ${x.user}`},
-                {label: 'Strongly Agree', value: `4 ${x.user}`},
+              {label: 'Strongly Disagree', value: `0 ${x.user}`},
+              {label: 'Disagree', value: `1 ${x.user}`},
+              {label: 'Neutral', value: `2 ${x.user}`},
+              {label: 'Agree', value: `3 ${x.user}`},
+              {label: 'Strongly Agree', value: `4 ${x.user}`},
             ],
           })
           console.log(`pushed to newQuestions. ${newQuestions}`)
@@ -152,8 +157,8 @@ class RoundSurveyForm extends React.Component {
       })
     }
     return (<div style={{width: '100%'}}>
-      {!readOnly && <p> IMPORTANT: Finishing the survey is <b>required</b> to participate in this experiment.</p>}
-      {!readOnly && <p> If you do not finish the survey, <b>you will NOT be paid for this task.</b> </p>}
+        {!readOnly && <p> IMPORTANT: Finishing the survey is <b>required</b> to participate in this experiment.</p>}
+        {!readOnly && <p> If you do not finish the survey, <b>you will NOT be paid for this task.</b></p>}
         <form className='form' style={{paddingBottom: '5vh'}} onSubmit={this.props.handleSubmit}>
           <Container>
             <Row>
@@ -171,7 +176,7 @@ class RoundSurveyForm extends React.Component {
                     surveyType={surveyType}
                     team={team}
                   />
-              </div>
+                </div>
               </Col>
             </Row>
             {!readOnly && <Row>
